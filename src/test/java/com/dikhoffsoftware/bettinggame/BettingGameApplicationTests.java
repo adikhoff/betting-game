9643ba0a3cd43728aaab24f188731f1a76907572
Arjan (A.) Dikhoff <arjan.dikhoff@ing.com>
2020-11-27 12:43:07 +0100

    Spring boot initial

---
+++ src/test/java/com/dikhoffsoftware/bettinggame/BettingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dikhoffsoftware.bettinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
