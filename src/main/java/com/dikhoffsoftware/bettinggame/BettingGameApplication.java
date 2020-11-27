907b5d4f0348e73887e6d635793d3dcea9abe0ac
adikhoff <dikhoff@gmail.com>
2020-11-27 16:09:33 +0100

    Implement api. Use yaml for configuration

--- src/main/java/com/dikhoffsoftware/bettinggame/BettingGameApplication.java
+++ src/main/java/com/dikhoffsoftware/bettinggame/BettingGameApplication.java
@@ -1,13 +1,20 @@
 package com.dikhoffsoftware.bettinggame;
 
+import com.dikhoffsoftware.bettinggame.configuration.YamlPropertySourceFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:application.yml", factory = YamlPropertySourceFactory.class)
+@Slf4j
 public class BettingGameApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BettingGameApplication.class, args);
-	}
+    public static void main(String[] args) {
+        log.info("Starting application...");
+        SpringApplication.run(BettingGameApplication.class, args);
+        log.info("Finished application...");
+    }
 
 }
