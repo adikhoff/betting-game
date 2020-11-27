57b62a89bf84afeda99534d6e1979ca65bf24b1f
adikhoff <dikhoff@gmail.com>
2020-11-27 16:09:33 +0100

    Implement api. Use yaml for configuration

---
+++ src/test/java/com/dikhoffsoftware/bettinggame/web/TestAPIController.java
@@ -0,0 +1,14 @@
+package com.dikhoffsoftware.bettinggame.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestAPIController {
+
+    @Test
+    public void testHome() {
+
+    }
+
+}
