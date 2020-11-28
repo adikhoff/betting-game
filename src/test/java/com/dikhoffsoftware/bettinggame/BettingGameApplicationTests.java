e5f0726ab449819701250253006b80a5acbdcb41
adikhoff <dikhoff@gmail.com>
2020-11-28 23:28:26 +0100

    Configure logging

--- src/test/java/com/dikhoffsoftware/bettinggame/BettingGameApplicationTests.java
+++ src/test/java/com/dikhoffsoftware/bettinggame/BettingGameApplicationTests.java
@@ -3,9 +3,11 @@ package com.dikhoffsoftware.bettinggame;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @Slf4j
 @SpringBootTest
+@ActiveProfiles("test")
 class BettingGameApplicationTests {
 
 	@Test
