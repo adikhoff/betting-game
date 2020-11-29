4f34c4c825fd110fd0d6bb58cfd3768000edf84b
adikhoff <dikhoff@gmail.com>
2020-11-29 19:41:13 +0100

    Make http gets possible. Create mock json server.

--- src/main/frontend/src/setupTests.js
+++ src/main/frontend/src/setupTests.js
@@ -5,5 +5,8 @@
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import fetch from './__mocks__/fetch';
+
+global.fetch = fetch;
 
 configure({ adapter: new Adapter()})
