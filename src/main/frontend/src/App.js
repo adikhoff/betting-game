f84207fbf407c2e2496f68fcb583ae82e5e0545d
Arjan (A.) Dikhoff <arjan.dikhoff@ing.com>
2020-11-29 16:40:08 +0100

    First little steps toward implementing frontend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {};
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Welkom in de familie app</h1>
+                <div id="login-form">
+                    <form>
+                        <input id="username" />
+                        <input id="password" />
+                        <button id="login-btn">Inloggen</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/main/frontend/src/App.test.js
+++ src/main/frontend/src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+  });
+
+  it('renders the title', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toBe('Welkom in de familie app')
+  });
+
+  it('has state', () => {
+    const wrapper = shallow(<App />);
+    const state = wrapper.state();
+
+    expect(state).not.toBeNull();
+  });
+
+  it('renders the form', () => {
+    const wrapper = shallow(<App />);
+    const form = wrapper.find('form');
+
+    expect(form.find('#username')).not.toBeNull();
+    expect(form.find('#password')).not.toBeNull();
+    expect(form.find('#login-btn').text()).toBe('Inloggen');
+  });
+
+  it('submits the form on login', () => {
+    //todo
+  });
+
+
 });
+
+
+
